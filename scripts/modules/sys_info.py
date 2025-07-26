14478cf7f13380d03c7dd8165e97108369188229
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2025-07-26 15:27:03 -0500

    Revert "bpy utils"
    This reverts commit cd3f14a418bddb888ec118d44d86de7ba7ce5a39.

--- scripts/modules/sys_info.py
+++ scripts/modules/sys_info.py
@@ -96,7 +96,7 @@ def write_sysinfo(filepath):
             output.write("datafiles: %r\n" % (bpy.utils.user_resource('DATAFILES')))
             output.write("config: %r\n" % (bpy.utils.user_resource('CONFIG')))
             output.write("scripts: %r\n" % (bpy.utils.user_resource('SCRIPTS')))
-            output.write("extensions: %r\n" % (bpy.utils.user_resource('EXTENSIONS')))
+            output.write("autosave: %r\n" % (bpy.utils.user_resource('AUTOSAVE')))
             output.write("tempdir: %r\n" % (bpy.app.tempdir))
 
             output.write(title("FFmpeg"))
@@ -208,7 +208,6 @@ def write_sysinfo(filepath):
                 output.write("Maximum Vertex Image Units:\t%d\n" % gpu.capabilities.max_textures_vert_get())
                 output.write("Maximum Fragment Image Units:\t%d\n" % gpu.capabilities.max_textures_frag_get())
                 output.write("Maximum Pipeline Image Units:\t%d\n" % gpu.capabilities.max_textures_get())
-                output.write("Maximum Image Units:\t%d\n" % gpu.capabilities.max_images_get())
 
                 output.write("\nFeatures:\n")
                 output.write("Compute Shader Support:               \t%d\n" %
