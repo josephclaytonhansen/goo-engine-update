aa18a9d9f86b9af9c4b3fded245604c12cc66750
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2025-08-04 10:53:45 -0500

    tooltips

--- source/blender/blenkernel/BKE_image.h
+++ source/blender/blenkernel/BKE_image.h
@@ -153,6 +153,15 @@ bool BKE_image_has_ibuf(struct Image *ima, struct ImageUser *iuser);
 struct ImBuf *BKE_image_acquire_ibuf(struct Image *ima, struct ImageUser *iuser, void **r_lock);
 void BKE_image_release_ibuf(struct Image *ima, struct ImBuf *ibuf, void *lock);
 
+/**
+ * Return image buffer of preview for given image
+ * r_width & r_height are optional and return the _original size_ of the image.
+ */
+struct ImBuf *BKE_image_preview(struct Image *ima,
+                                short max_size,
+                                short *r_width,
+                                short *r_height);
+
 struct ImagePool *BKE_image_pool_new(void);
 void BKE_image_pool_free(struct ImagePool *pool);
 struct ImBuf *BKE_image_pool_acquire_ibuf(struct Image *ima,
