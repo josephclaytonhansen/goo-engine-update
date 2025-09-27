5cbbd29f3a389c673d0f557e95e253b854bb01da
Cody Winchester <codywinchart@gmail.com>
2025-09-27 10:46:11 -0500

    4.2 Merge fixes

--- source/blender/blenkernel/BKE_node.hh
+++ source/blender/blenkernel/BKE_node.hh
@@ -967,6 +967,15 @@ void BKE_nodetree_remove_layer_n(bNodeTree *ntree, Scene *scene, int layer_index
 #define SH_NODE_MIX 713
 #define SH_NODE_BSDF_RAY_PORTAL 714
 
+#define SH_NODE_SHADER_INFO 800
+#define SH_NODE_SCREENSPACE_INFO 801
+#define SH_NODE_SDF_PRIMITIVE 802
+#define SH_NODE_SDF_OP 803
+#define SH_NODE_SDF_VECTOR_OP 804
+#define SH_NODE_SDF_NOISE 805
+#define SH_NODE_SET_DEPTH 806
+#define SH_NODE_CURVATURE 807
+
 /** \} */
 
 /* -------------------------------------------------------------------- */
--- source/blender/makesdna/DNA_userdef_types.h
+++ source/blender/makesdna/DNA_userdef_types.h
@@ -760,6 +760,7 @@ typedef struct UserDef_Experimental {
 
   char use_grease_pencil_version3_convert_on_load;
   char use_animation_baklava;
+  char _pad[7];
   /** `makesdna` does not allow empty structs. */
 } UserDef_Experimental;
 
